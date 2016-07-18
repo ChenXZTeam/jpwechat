8ac5a6d7738d5d9d48719555fc3d0b545eb90673
xchg091 <1241252907@qq.com>
2016-07-18 18:28:00 +0800

    修改了后台信息显示代码

--- src/com/solar/tech/controller/wechat/PlanTekController.java
+++ src/com/solar/tech/controller/wechat/PlanTekController.java
@@ -27,51 +27,59 @@ public class PlanTekController {
 		Map<String, Object> map = new HashMap<String, Object>();
 		//PlanTekServ.findHB(chufCity, daodCity, cangW, dateTime);
 		List<FlightInfo> fliL = PlanTekServ.findHB(chufCity, daodCity, cangW, dateTime);
-		List<FlightInfo> tempFlil = new ArrayList<FlightInfo>();
+		List<FlightInfo> tempFlil=removeRepeat(fliL);
+		//List<FlightInfo> tempFlil = new ArrayList<FlightInfo>();
 		//SeatInfo info=new SeatInfo();
-		if(fliL != null && fliL.size() > 0){
-			int i=0;
-			for(FlightInfo f : fliL){
+		if(tempFlil != null && tempFlil.size() > 0){
+			for(FlightInfo f : tempFlil){
 				if(f.getSeatList().size()!=0){
-					System.out.println(i+". 航班号：" + f.getFlightNo() + "， 出发城市：" + f.getOrgCity() + " ( 起飞时间：" + f.getDepTime() + " ) " + "， 到达城市: " + f.getDstCity() + " ( 到达时间：" + f.getArrTime() + " ) ，机型：" + f.getPlaneStyle());				
-					tempFlil.add(f);
-					//int j=0;
+					System.out.println("航班号：" + f.getFlightNo() + "， 出发城市：" + f.getOrgCity() + " ( 起飞时间：" + f.getDepTime() + " ) " + "， 到达城市: " + f.getDstCity() + " ( 到达时间：" + f.getArrTime() + " ) ，机型：" + f.getPlaneStyle());				
+					//tempFlil.add(f);
 					int SumTecikNum=0;
 					for(SeatInfo info : f.getSeatList()){	
 						if(SeatUtils.getSeatType(info.getBasicCabin()).equals(cangW)){
-							//System.out.println("座位类型："+info.getCangwei() + " (" + SeatUtils.getSeatType(info.getBasicCabin()) + ")" + "， 剩余座位: " + SeatUtils.getSeatNum(info.getCangwei_data()) +"， 单程: " + info.getOnewayPrice() + "元\t\t 往返: " + info.getRoundtripPrice()+"元");
+							System.out.println("座位类型："+info.getCangwei() + " (" + SeatUtils.getSeatType(info.getBasicCabin()) + ")" + "， 剩余座位: " + SeatUtils.getSeatNum(info.getCangwei_data()) +"， 单程: " + info.getOnewayPrice() + "元\t\t 往返: " + info.getRoundtripPrice()+"元");
 							//System.out.println(j+". 剩余的票数：" + " (" + SeatUtils.getSeatType(info.getBasicCabin()) + ") " +SeatUtils.getSeatNum(info.getCangwei_data()));							
 							int TecikNum=SeatUtils.getSeatNum(info.getCangwei_data());
 							SumTecikNum+=TecikNum;
-							//System.out.println();
-							//j++;
 						}
 					}
-					System.out.println("剩余的票数：" + SumTecikNum);
-
+					System.out.println(cangW+"剩余的票数：" + SumTecikNum);
 				}
-				/*for(SeatInfo info : f.getSeatList()){					
-					//int j=0;
-					//System.out.println("座位类型："+info.getCangwei() + " (" + SeatUtils.getSeatType(cangW) + ")" + "， 剩余座位: " + SeatUtils.getSeatNum(info.getCangwei_data()) +"， 单程: " + info.getOnewayPrice() + "元\t\t 往返: " + info.getRoundtripPrice()+"元");
-					if(f.getSeatList().size()!=0 && SeatUtils.getSeatType(info.getBasicCabin()).equals(cangW)){
-						System.out.println("座位类型："+info.getCangwei() + " (" + SeatUtils.getSeatType(info.getBasicCabin()) + ")" + "， 剩余座位: " + SeatUtils.getSeatNum(info.getCangwei_data()) +"， 单程: " + info.getOnewayPrice() + "元\t\t 往返: " + info.getRoundtripPrice()+"元");
-						tempFlil.add(f);
-					}
-					//j++;
-				}*/
-				i++;
 			}
 		}
-		System.out.println("列表的长度："+fliL.size());
-		System.out.println("列表的长度："+tempFlil.size());
-		
-		if(tempFlil.size()==0){
-			map.put("msg", 0);
-		}else{
-			map.put("msg", 1);
-			map.put("listDate", tempFlil);
+		if(tempFlil != null && tempFlil.size() > 0){
+			System.out.println("列表的长度："+tempFlil.size());
+			if(tempFlil.size()==0){
+				map.put("msg", 0);
+			}else{
+				map.put("msg", 1);
+				map.put("listDate", tempFlil);
+			}
 		}
 		//System.out.println("出发城市："+chufCity+"，到达城市："+daodCity+"，舱位："+cangW+"，出发时间："+dateTime);
 		return map;
+	}
+	
+	// 剔除重复的数据
+	private List<FlightInfo> removeRepeat(List<FlightInfo> list){
+		List<FlightInfo> resultList = new ArrayList<FlightInfo>();
+		
+		if(list != null && list.size() > 0){
+			for(FlightInfo info : list){
+				boolean flag = true;
+				for(FlightInfo rInfo : resultList){
+					if(info.getFlightNo().equals(rInfo.getFlightNo())){
+						flag = false;
+						break;
+					}
+				}
+				if(flag){
+					resultList.add(info);
+				}
+			}
+		}
+		
+		return resultList;
 	}
 }
--- src/com/solar/tech/service/PlanTekService.java
+++ src/com/solar/tech/service/PlanTekService.java
@@ -1,15 +1,11 @@
 package com.solar.tech.service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.solar.tech.utils.SeatUtils;
 import com.solar.tech.utils.OptimizeECUtils;
 import com.solar.tech.bean.entity.FlightInfo;
-import com.solar.tech.bean.entity.SeatInfo;
 
 /**
  * @title PlanTekService 处理机票的查询、预定的类
@@ -27,7 +23,7 @@ public class PlanTekService {
 	public List<FlightInfo> findHB(String chufCity,String daodCity,String cangW, String dateTime){
 		//SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		//String startDate = fmt.format(new Date());
-		List<FlightInfo> list = new OptimizeECUtils().query("CAN", "PEK", dateTime, "CZ"); // 传入出发城市、到达城市、日期及航空公司代码		
+		List<FlightInfo> list = new OptimizeECUtils().query("CAN", "PEK", "2016-07-30", "CZ"); // 传入出发城市、到达城市、日期及航空公司代码
 		return list;
 	}
 }
