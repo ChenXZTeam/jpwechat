dba1d4a3c75788bace15c394c7d5d0666c304e3f
wakeup1027 <446113922@qq.com>
2016-07-27 17:47:44 +0800

    修改方法的存放位置

--- src/com/solar/tech/controller/wechat/PlanTekController.java
+++ src/com/solar/tech/controller/wechat/PlanTekController.java
@@ -1,6 +1,5 @@
 package com.solar.tech.controller.wechat;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,16 +28,12 @@ public class PlanTekController {
 		System.out.println(daodCity);
 		System.out.println(cangW);
 		System.out.println(dateTime);
-		//PlanTekServ.findHB(chufCity, daodCity, cangW, dateTime);
-		List<FlightInfo> fliL = PlanTekServ.findHB(chufCity, daodCity, cangW, dateTime);
-		List<FlightInfo> tempFlil=removeRepeat(fliL);
-		//List<FlightInfo> tempFlil = new ArrayList<FlightInfo>();
-		//SeatInfo info=new SeatInfo();
+		List<FlightInfo> fliL = PlanTekServ.findHB(chufCity, daodCity, "CZ", dateTime, null);
+		List<FlightInfo> tempFlil = PlanTekServ.removeRepeat(fliL);
 		if(tempFlil != null && tempFlil.size() > 0){
 			for(FlightInfo f : tempFlil){
 				if(f.getSeatList().size()!=0){
 					System.out.println("航班号：" + f.getFlightNo() + "， 出发城市：" + f.getOrgCity() + " ( 起飞时间：" + f.getDepTime() + " ) " + "， 到达城市: " + f.getDstCity() + " ( 到达时间：" + f.getArrTime() + " ) ，机型：" + f.getPlaneStyle());				
-					//tempFlil.add(f);
 					int SumTecikNum=0;
 					for(SeatInfo info : f.getSeatList()){	
 						if(SeatUtils.getSeatType(info.getBasicCabin()).equals(cangW)){
@@ -61,29 +56,7 @@ public class PlanTekController {
 				map.put("listDate", tempFlil);
 			}
 		}
-		//System.out.println("出发城市："+chufCity+"，到达城市："+daodCity+"，舱位："+cangW+"，出发时间："+dateTime);
 		return map;
 	}
-	
-	// 剔除重复的数据
-	private List<FlightInfo> removeRepeat(List<FlightInfo> list){
-		List<FlightInfo> resultList = new ArrayList<FlightInfo>();
-		
-		if(list != null && list.size() > 0){
-			for(FlightInfo info : list){
-				boolean flag = true;
-				for(FlightInfo rInfo : resultList){
-					if(info.getFlightNo().equals(rInfo.getFlightNo())){
-						flag = false;
-						break;
-					}
-				}
-				if(flag){
-					resultList.add(info);
-				}
-			}
-		}
-		
-		return resultList;
-	}
+
 }
--- src/com/solar/tech/service/PlanTekService.java
+++ src/com/solar/tech/service/PlanTekService.java
@@ -1,9 +1,11 @@
 package com.solar.tech.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.solar.tech.utils.OptimizeECUtils;
 import com.solar.tech.bean.entity.FlightInfo;
 
@@ -20,10 +22,33 @@ public class PlanTekService {
 	 * @title 传入出发城市、到达城市、日期、航班号及舱位，返回该舱位可用座位数 
 	 */
 	//传入出发城市、到达城市、日期及航空公司代码  例如："CAN", "PEK", "2016-12-01", "CZ"
-	public List<FlightInfo> findHB(String chufCity,String daodCity,String cangW, String dateTime){//cancelPnr
+	public List<FlightInfo> findHB(String chufCity,String daodCity,String airCode, String dateTime, String filgNo){//cancelPnr
 		//SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		//String startDate = fmt.format(new Date());
-		List<FlightInfo> list = new OptimizeECUtils().query(chufCity, daodCity, dateTime, "CZ"); // 传入出发城市、到达城市、日期及航空公司代码
+		List<FlightInfo> list = new OptimizeECUtils().query(chufCity, daodCity, dateTime, airCode , filgNo); // 传入出发城市、到达城市、日期及航空公司代码
 		return list;
+	}
+	
+	
+	// 剔除重复的数据
+	public List<FlightInfo> removeRepeat(List<FlightInfo> list){
+		List<FlightInfo> resultList = new ArrayList<FlightInfo>();
+		
+		if(list != null && list.size() > 0){
+			for(FlightInfo info : list){
+				boolean flag = true;
+				for(FlightInfo rInfo : resultList){
+					if(info.getFlightNo().equals(rInfo.getFlightNo())){
+						flag = false;
+						break;
+					}
+				}
+				if(flag){
+					resultList.add(info);
+				}
+			}
+		}
+		
+		return resultList;
 	}
 }
