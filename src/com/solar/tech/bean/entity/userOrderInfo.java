aca48cc1598936531fdd8a97bd2e86b8f18bda68
wakeup1027 <446113922@qq.com>
2016-07-29 18:12:03 +0800

    增加订单表中创建时间的字段，并使用这个字段进行排列

--- src/com/solar/tech/bean/entity/userOrderInfo.java
+++ src/com/solar/tech/bean/entity/userOrderInfo.java
@@ -6,7 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Table; 
 
 import org.hibernate.annotations.GenericGenerator;
 
